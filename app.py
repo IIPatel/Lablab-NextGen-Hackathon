44536e3de1a7d786d0cc5e26e9bc81ce6874de9b
Ibrahim Patel <ipatel.me@gmail.com>
2024-01-21 22:53:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def text_to_speech(input_text):
     ).predict_by_bytes(
         input_text.encode(), input_type="text", inference_params=inference_params
     )
-    audio_base64 = model_prediction.outputs[0].data.audio.base6
+    audio_base64 = model_prediction.outputs[0].data.audio.base64
     return audio_base64
 
 def main():
